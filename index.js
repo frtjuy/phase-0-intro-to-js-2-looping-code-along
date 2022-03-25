84d432190475ef627f37aefad56cdce8ce1c145c
Alexander Edwards <alexd.edwards1999@gmail.com>
2022-03-24 20:22:49 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+let names = []
+let card = []
+
+function writeCards(names, event) {
+    for (let i=0; i < names.length; i++) {
+        let name = names
+        card.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+        debugger;
+    }
+    return card
+}
+
+function countDown (num) {
+    for (let i=num; i > -1 ; --i) {
+        console.log(i);
+    }
+}
